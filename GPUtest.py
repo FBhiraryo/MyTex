35b00628a2f96c0c7e91f6d71928a7fb05208a50
FBhiraryo <fbhr05@outlook.jp>
2026-01-17 04:16:42 +0900

    nothing

--- GPUtest.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-if tf.config.list_physical_devices('GPU'):
-    print("GPU is available. TensorFlow will use it.")
-    print(tf.config.list_physical_devices('GPU'))
-else:
-    print("No GPU devices found.")
